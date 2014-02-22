05bdef5ec6b167d2afa213b65c1178f76ce77955
Leo Unglaub <leo.unglaub@2create.at>
2014-02-22 15:40:31 +0100

    Change the copyright header
    Signed-off-by: Leo Unglaub <leo.unglaub@2create.at>

--- system/modules/ac_core/classes/AcCoreAjax.php
+++ system/modules/ac_core/classes/AcCoreAjax.php
@@ -1,49 +1,26 @@
-<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+<?php
 
 /**
  * Contao Open Source CMS
- * Copyright (C) 2005-2011 Leo Feyer
+ * Copyright (c) 2005-2014 Leo Feyer
  *
- * Formerly known as TYPOlight Open Source CMS.
- *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at <http://www.gnu.org/licenses/>.
- *
- * PHP version 5
- * @copyright  Leo Unglaub 2012
- * @author     Leo Unglaub <leo@leo-unglaub.net>
- * @package    ac_core
- * @license    LGPL
- * @filesource
+ * @copyright	Leo Unglaub 2014
+ * @author		Leo Unglaub <leo@leo-unglaub.net>
+ * @package		ac_core
+ * @license		GPL
  */
 
 
 /**
  * Class AcCoreAjax
- * 
- * Contains methods to catch ajax calls for the auto completer and 
+ * Contains methods to catch ajax calls for the auto completer and
  * pass them to the Hook "getAutoCompleterChoices"
- * 
- * @copyright  Leo Unglaub 2012
- * @author     Leo Unglaub <leo@leo-unglaub.net>
- * @package    ac_core
  */
 class AcCoreAjax extends Controller
 {
 	/**
 	 * Manage all ajax calls for the auto completer
-	 * 
+	 *
 	 * @param void
 	 * @return string|bool
 	 */
@@ -83,5 +60,3 @@ class AcCoreAjax extends Controller
 		}
 	}
 }
-
-?>
--- system/modules/ac_core/classes/AutoCompleter.php
+++ system/modules/ac_core/classes/AutoCompleter.php
@@ -1,41 +1,19 @@
-<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+<?php
 
 /**
  * Contao Open Source CMS
- * Copyright (C) 2005-2011 Leo Feyer
+ * Copyright (c) 2005-2014 Leo Feyer
  *
- * Formerly known as TYPOlight Open Source CMS.
- *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at <http://www.gnu.org/licenses/>.
- *
- * PHP version 5
- * @copyright  Leo Unglaub 2012
- * @author     Leo Unglaub <leo@leo-unglaub.net>
- * @package    ac_core
- * @license    LGPL
- * @filesource
+ * @copyright	Leo Unglaub 2014
+ * @author		Leo Unglaub <leo@leo-unglaub.net>
+ * @package		ac_core
+ * @license		GPL
  */
 
 
 /**
  * Class AutoCompleter
  * Provide mathods for the auto completer
- * 
- * @copyright  Leo Unglaub 2012
- * @author     Leo Unglaub <leo@leo-unglaub.net>
- * @package    ac_core
  */
 class AutoCompleter extends Controller
 {
@@ -69,7 +47,7 @@ class AutoCompleter extends Controller
 	 */
 	protected $arrConfigOptions = array
 	(
-		'minLength', 'markQuery', 'width', 'maxChoices', 'visibleChoices', 'className', 'zIndex', 'delay', 'autoSubmit', 'overflow', 
+		'minLength', 'markQuery', 'width', 'maxChoices', 'visibleChoices', 'className', 'zIndex', 'delay', 'autoSubmit', 'overflow',
 		'overflowMargin', 'selectFirst', 'forceSelect', 'selectMode', 'multiple', 'separator', 'autoTrim', 'allowDupes', 'cache',
 		'relative', 'indicatorClass'
 	);
@@ -88,7 +66,7 @@ class AutoCompleter extends Controller
 
 	/**
 	 * set a object property or a config option
-	 * 
+	 *
 	 * @param string $strKey
 	 * @param mixed $varValue
 	 * @return void
@@ -124,7 +102,7 @@ class AutoCompleter extends Controller
 
 	/**
 	 * return a object property or a config option
-	 * 
+	 *
 	 * @param string $strKey
 	 * @return mixed
 	 */
@@ -228,10 +206,8 @@ class AutoCompleter extends Controller
 
 			return;
 		}
-		
+
 		// add the HTML5 version
 		$GLOBALS['TL_HEAD'][] = '<script>' . $strBuild . '</script>';
 	}
 }
-
-?>
