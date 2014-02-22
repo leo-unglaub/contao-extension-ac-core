9097679f3ccc0a9eb04e5465f93f3d12dc3d765f
Leo Unglaub <leo.unglaub@2create.at>
2014-02-22 15:34:14 +0100

    Restructure the file structure
     * add the autoload* files
     * move all classes to classes/
     * remove an old .gitignore
    
    Signed-off-by: Leo Unglaub <leo.unglaub@2create.at>

---
+++ system/modules/ac_core/config/autoload.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2014 Leo Feyer
+ *
+ * @package Ac_core
+ * @link    https://contao.org
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+	// Classes
+	'AcCoreAjax'    => 'system/modules/ac_core/classes/AcCoreAjax.php',
+	'AutoCompleter' => 'system/modules/ac_core/classes/AutoCompleter.php',
+));
